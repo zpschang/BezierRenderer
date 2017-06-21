77bb7facf89f43aaa0da4db1057c61d39b1ee67f
zpschang <zps112358@126.com>
2017-06-21 23:14:17 +0800

    6.21.5

---
+++ Bezier/SphereObject.cpp
@@ -0,0 +1,9 @@
+//
+//  SphereObject.cpp
+//  Bezier
+//
+//  Created by 张蒲石 on 2017/6/21.
+//  Copyright © 2017年 张蒲石. All rights reserved.
+//
+
+#include "SphereObject.hpp"
---
+++ Bezier/SphereObject.hpp
@@ -0,0 +1,25 @@
+//
+//  SphereObject.hpp
+//  Bezier
+//
+//  Created by 张蒲石 on 2017/6/21.
+//  Copyright © 2017年 张蒲石. All rights reserved.
+//
+
+#ifndef SphereObject_hpp
+#define SphereObject_hpp
+
+#include <stdio.h>
+#include "object.hpp"
+
+class SphereObject
+{
+public:
+    Point3d center;
+    double radius;
+    Object::Info find_intersection(Ray ray);
+    void set_attribute(std::string info);
+};
+
+
+#endif /* SphereObject_hpp */
